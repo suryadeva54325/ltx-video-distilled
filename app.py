6282f2a9c58a1e5e4b5c2deebc5d74bf67ee2079
suryadeva54325 <suryadeva54325@gmail.com>
2025-06-08 14:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -483,4 +483,4 @@ if __name__ == "__main__":
     if os.path.exists(models_dir) and os.path.isdir(models_dir):
         print(f"Model directory: {Path(models_dir).resolve()}")
     
-    demo.queue().launch(debug=True, share=False, mcp_server=True)
+    demo.queue().launch(debug=True, share=True, mcp_server=True)
